5d4fa1ae6267feb9a8b4f361e35d8e786de6380e
Agh42 <bitbucket@koderman.de>
2021-04-28 00:32:48 +0200

    rename files

--- data/BSI-IT-Baseline/script/convert.sh
+++ data/BSI-IT-Baseline/script/convert.sh
@@ -9,17 +9,17 @@ cat src/verinice.xml | xq . > build/verinice.json
 jq '.. | .children? | select(.) | .[] | .extId? as $extid | .extObjectType as $type | .syncAttribute? |  map(select(.name|contains("_name"))|.value)[0] as $name | map(select(.name|contains("browser_content"))|.value)[0] as $text | map(select(.name|contains("_id"))|.value)[0] as $id | [{name: $name, text: $text, gsid: $id, extId: $extid, type: $type}]  ' build/verinice.json >  build/nodes.json 
 
 # parent-child relations: level 1:
-jq '."ns3:syncRequest".syncData.syncObject | .extId? as $extId | .extObjectType as $parentType | .children[] | .extId as $childId | .extObjectType as $childType | {parentId: $extId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_links-l1.json
+jq '."ns3:syncRequest".syncData.syncObject | .extId? as $extId | .extObjectType as $parentType | .children[] | .extId as $childId | .extObjectType as $childType | {parentId: $extId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_rels-l1.json
 
 # level 2:
-jq '."ns3:syncRequest".syncData.syncObject.children[] | .extId as $parentId | .extObjectType as $parentType | .children[] |  .extId? as $childId | .extObjectType as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_links-l2.json
+jq '."ns3:syncRequest".syncData.syncObject.children[] | .extId as $parentId | .extObjectType as $parentType | .children[] |  .extId? as $childId | .extObjectType as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_rels-l2.json
 
 # level 3:
-jq '."ns3:syncRequest".syncData.syncObject.children[].children[] |  .extId? as $parentId | .extObjectType as $parentType | .children[] | .extId as $childId | .extObjectType as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child-links-l3.json
+jq '."ns3:syncRequest".syncData.syncObject.children[].children[] |  .extId? as $parentId | .extObjectType as $parentType | .children[] | .extId as $childId | .extObjectType as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child-rels-l3.json
 
 # level 4:
-jq '."ns3:syncRequest".syncData.syncObject.children[].children[].children? | select(.) | .[] | select(.) | .extId as $parentId | .extObjectType as $parentType | .children? | select(.) | .[] | select(.) | .extId? as $childId | .extObjectType? as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_links-l4.json
+jq '."ns3:syncRequest".syncData.syncObject.children[].children[].children? | select(.) | .[] | select(.) | .extId as $parentId | .extObjectType as $parentType | .children? | select(.) | .[] | select(.) | .extId? as $childId | .extObjectType? as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_rels-l4.json
 
 # level 5:
-jq '."ns3:syncRequest".syncData.syncObject.children[].children[].children? | select(.) | .[] | select(.) | .children? | select(.) | .[] | select(.) | .extId? as $parentId | .extObjectType? as $parentType | .children? | select(.) | .[] | select(.) | .extId? as $childId | .extObjectType? as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_links-l5.json
+jq '."ns3:syncRequest".syncData.syncObject.children[].children[].children? | select(.) | .[] | select(.) | .children? | select(.) | .[] | select(.) | .extId? as $parentId | .extObjectType? as $parentType | .children? | select(.) | .[] | select(.) | .extId? as $childId | .extObjectType? as $childType | {parentId: $parentId, parentType: $parentType, childId: $childId, childType: $childType}' build/verinice.json > build/child_rels-l5.json
 
